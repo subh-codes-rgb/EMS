16fb3316725ca4ef2b93486fdd0ea829e80e8e80
Subham Das <subhamd3005@gmail.com>
2026-07-29 19:02:58 +0530

    Login form completed

--- client/src/components/LoginLeftSide.jsx
+++ client/src/components/LoginLeftSide.jsx
@@ -6,8 +6,8 @@ const LoginLeftSide = () => {
         <div className="absolute -top-30 -left-30 w-72 h-72 bg-indigo-500/20 rounded-full blur-3xl">
         </div>
 
-        <div className="relatibe z-10 flex flex-col items-start justify-center p-12 lg:p-20 w-full h-full">
-            <h1 className="text-4xl lg:text-5xl font-medium text-white mb-6 leading-tight tracking-tight'">Employee <br /> Management System</h1>
+        <div className="relative z-10 flex flex-col items-start justify-center p-12 lg:p-20 w-full h-full">
+            <h1 className="text-4xl lg:text-5xl font-medium text-white mb-6 leading-tight tracking-tight">Employee <br /> Management System</h1>
             <p className="text-slate-400 text-lg max-w-md leading-relaxed">Streamline workforce management with effortless attendance tracking, payroll automation, and secure employee management.</p>
         </div>
     </div>
